d403ad70719c4b9b7029fde1700a91b4cb8cc985
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-03 00:12:13 -0800

    added a selector

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,5 +1,11 @@
+import { selecTasks, selectFilter } from "@/Redux/features/task/taskSlice"
+import { useAppSelector } from "@/Redux/hook"
 
 export default function Task() {
+  const tasks = useAppSelector(selecTasks)
+  const filter = useAppSelector(selectFilter)
+  console.log(tasks)
+  console.log(filter)
   return (
     <div>
       <h1>please compleate the task</h1>
