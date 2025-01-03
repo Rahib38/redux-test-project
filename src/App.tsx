a0909ed38fb6cdafbc47b7ee833ee13bd34faf2b
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-02 20:04:56 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { decrement, increment } from "./Redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./Redux/hook";
+
+function App() {
+  const dispatch = useAppDispatch();
+
+
+  const { count } = useAppSelector((state) => state.counter);
+
+  const handleIncrement = (amount:number) => {
+    dispatch(increment(amount));
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+  return (
+    <>
+      <div>
+        <h1>Counter with Redux</h1>
+        <Button onClick={()=>handleIncrement(5)}>Increment 5</Button>
+        <Button onClick={()=>handleIncrement(1)}>Increment</Button>
+        <div>{count}</div>
+        <Button onClick={handleDecrement}>Decrement</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
