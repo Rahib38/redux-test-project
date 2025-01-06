6edd17f5942e3144162a37b8889e776a1e3ffd9d
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-05 18:15:42 -0800

    added filter

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,20 +1,55 @@
 import TaskCard from "@/components/module/tasks/TaskCard";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { AddTaskModel } from "@/Redux/features/task/AddTaskModel";
-import { selecTasks, selectFilter } from "@/Redux/features/task/taskSlice";
-import { useAppSelector } from "@/Redux/hook";
+import {
+  selecTasks,
+  selectFilter,
+  updateFilter,
+} from "@/Redux/features/task/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/Redux/hook";
 
 export default function Task() {
   const tasks = useAppSelector(selecTasks);
   const filter = useAppSelector(selectFilter);
+  const dispatch = useAppDispatch();
+  console.log(dispatch)
   console.log(tasks);
   console.log(filter);
   return (
-    <div>
-      <div className="flex justify-between items-center">
-        <h1>please compleate the task</h1>
+    <div className="mt-20">
+      <div className="flex justify-end items-center gap-5">
+        <h1 className="mr-auto">please compleate the task</h1>
+        <Tabs defaultValue="all">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("all"))}
+              value="all"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("low"))}
+              value="low"
+            >
+              Low
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("medium"))}
+              value="medium"
+            >
+              Medium
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("high"))}
+              value="high"
+            >
+              High
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
         <AddTaskModel></AddTaskModel>
       </div>
-      <div className="gap-4 space-y-4">
+      <div className="gap-4 space-y-4 mt-12">
         {tasks.map((task) => (
           <TaskCard task={task} key={task.id}></TaskCard>
         ))}
